6b05a6496cfbf85bbed02377fdfe5428cc033e61
Atiar-Rahman <atiar1709@gmail.com>
2026-04-02 09:05:44 +0600

    users app create for custom user create

--- task1/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from task1.models import Student
-
-
-class StudentSerializer(ModelSerializer):
-    class Meta:
-        model = Student
-        fields = '__all__'
-        read_only_fields = ['created_at','updated_at','status',]
