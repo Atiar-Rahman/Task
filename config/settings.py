01465a39f43f332653802cc6ba371c25a691a3f3
Atiar-Rahman <atiar1709@gmail.com>
2026-04-02 09:33:45 +0600

    custom user model and manager setup

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+#custom user model add
+AUTH_USER_MODEL = 'users.User'
 # Application definition
 
 INSTALLED_APPS = [
--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from users.managers import CustomUser
 # Create your models here.
+
+class User(AbstractUser):
+    username=None #remove username
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    address = models.TextField(blank=True,null=True)
+    phone_number = models.CharField(max_length=15,blank=True,null=True)
+
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=[]
+
+    objects = CustomUser()
+
+    def __str__(self):
+        return self.email
